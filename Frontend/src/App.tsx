82b82b6537d1ca7fc44e70f9e393ef503c9c6dcf
michaelshikh <michaelshikh@microsoft.com>
2025-02-12 09:48:58 +0200

    cleaning ux

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,8 +4,10 @@ import { History } from "history";
 
 import { WorkloadClientAPI } from "@ms-fabric/workload-client";
 
-import { SampleWorkloadEditor, SamplePage } from "./components/SampleWorkloadEditor/SampleWorkloadEditor";
-import { Authentication } from './components/SampleWorkloadAuthEditor/SampleWorkloadAuthEditor';
+import {
+  SampleWorkloadEditor,
+  SamplePage,
+} from "./components/SampleWorkloadEditor/SampleWorkloadEditor";
 import { Panel } from "./components/SampleWorkloadPanel/SampleWorkloadPanel";
 import { SaveAsDialog } from "./components/SampleWorkloadCreateDialog/SampleWorkloadCreateDialog";
 import CustomItemSettings from "./components/CustomItemSettings/CustomItemSettings";
@@ -16,70 +18,78 @@ import CustomAbout from "./components/CustomItemSettings/CustomAbout";
 */
 
 interface AppProps {
-    history: History;
-    workloadClient: WorkloadClientAPI;
+  history: History;
+  workloadClient: WorkloadClientAPI;
 }
 
 export interface PageProps {
-    workloadClient: WorkloadClientAPI;
-    history?: History
-
+  workloadClient: WorkloadClientAPI;
+  history?: History;
 }
 
 export interface ContextProps {
-    itemObjectId?: string;
-    workspaceObjectId?: string
+  itemObjectId?: string;
+  workspaceObjectId?: string;
 }
 
 export function App({ history, workloadClient }: AppProps) {
-    return <Router history={history}>
-        <Switch>
-            {/* This is the routing to the Sample Workload Editor.
+  return (
+    <Router history={history}>
+      <Switch>
+        {/* This is the routing to the Sample Workload Editor.
                  Add your workload editor path here, and reference it in index.worker.ts  */}
-            <Route path="/sample-workload-editor/:itemObjectId">
-                <SampleWorkloadEditor
-                    workloadClient={workloadClient} data-testid="sample-workload-editor" />
-            </Route>
+        <Route path="/sample-workload-editor/:itemObjectId">
+          <SampleWorkloadEditor
+            workloadClient={workloadClient}
+            data-testid="sample-workload-editor"
+          />
+        </Route>
 
-            {/* This is the routing to the Sample Workload Frontend-ONLY experience.
+        {/* This is the routing to the Sample Workload Frontend-ONLY experience.
                  Add your workload creator path here, and reference it in index.worker.ts  */}
-            <Route path="/sample-workload-frontend-only">
-                <SampleWorkloadEditor
-                    workloadClient={workloadClient} data-testid="sample-workload-frontend-only" />
-            </Route>
+        <Route path="/sample-workload-frontend-only">
+          <SampleWorkloadEditor
+            workloadClient={workloadClient}
+            data-testid="sample-workload-frontend-only"
+          />
+        </Route>
 
-            {/* This is the routing to the Sample Workload Create Dialog experience, 
+        {/* This is the routing to the Sample Workload Create Dialog experience, 
                 where an Item will be saved and the Editor will be opened
                 Add your workload creator path here, and reference it in index.worker.ts  */}
-            <Route path="/sample-workload-create-dialog/:workspaceObjectId">
-                <SaveAsDialog
-                    workloadClient={workloadClient}
-                    isImmediateSave={true} data-testid="sample-workload-create-dialog" />
-            </Route>
+        <Route path="/sample-workload-create-dialog/:workspaceObjectId">
+          <SaveAsDialog
+            workloadClient={workloadClient}
+            isImmediateSave={true}
+            data-testid="sample-workload-create-dialog"
+          />
+        </Route>
 
-            {/* Routing to a sample Panel  */}
-            <Route path="/panel">
-                <Panel
-                    workloadClient={workloadClient} data-testid="sample-workload-panel" />
-            </Route>
+        {/* Routing to a sample Panel  */}
+        <Route path="/panel">
+          <Panel
+            workloadClient={workloadClient}
+            data-testid="sample-workload-panel"
+          />
+        </Route>
 
-            {/* Routing to a sample Page  */}
-            <Route path="/sample-page/:itemObjectId">
-                <SamplePage workloadClient={workloadClient} history={history} data-testid="sample-page" />
-            </Route>
+        {/* Routing to a sample Page  */}
+        <Route path="/sample-page/:itemObjectId">
+          <SamplePage
+            workloadClient={workloadClient}
+            history={history}
+            data-testid="sample-page"
+          />
+        </Route>
 
-            {/* Routing to an Authentication Editor */}
-            <Route path="/Authentication/:itemObjectId">
-                <Authentication workloadClient={workloadClient} history={history} data-testid="authentication-editor" />
-            </Route>
-
-            {/* Routing to Custom Item Settings */}
-            <Route path="/custom-item-settings">
-                <CustomItemSettings data-testid="custom-about" />
-            </Route>
-            <Route path="/custom-about">
-                <CustomAbout />
-            </Route>
-        </Switch>
-    </Router>;
+        {/* Routing to Custom Item Settings */}
+        <Route path="/custom-item-settings">
+          <CustomItemSettings data-testid="custom-about" />
+        </Route>
+        <Route path="/custom-about">
+          <CustomAbout />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
