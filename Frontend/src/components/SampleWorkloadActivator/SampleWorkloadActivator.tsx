21ad8c92f76b89668eb31ab38491dc38f3446cae
michaelshikh <michaelshikh@microsoft.com>
2025-02-21 17:09:10 +0200

    EventStream & Activator teasers

---
+++ Frontend/src/components/SampleWorkloadActivator/SampleWorkloadActivator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function ActivatorComponent() {
+    return (
+        <div>
+            <h2>Data Activator</h2>
+            <div>
+                <label>Coming soon! Stay tuned as we present Data Activator capabilities.</label>
+            </div>
+        </div>
+    );
+}
---
+++ Frontend/src/components/SampleWorkloadEventstream/SampleWorkloadEventstream.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function EventstreamComponent() {
+    return (
+        <div>
+            <h2>Eventstream</h2>
+            <div>
+                <label>Coming soon! Stay tuned as we present Eventstream capabilities.</label>
+            </div>
+        </div>
+    );
+}
