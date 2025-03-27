a8a00eb9bec23038d7efdf89cf4da02b075bbd55
michaelshikh <michaelshikh@microsoft.com>
2025-03-27 15:12:28 +0200

    Create EventHubClient

---
+++ Backend/src/Services/EventHubClient.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Fabric.Rti.workload.Backend.Services;
+
+public class EventHubClient : IEventHubClient
+{
+    private readonly EventHubProducerClient m_eventHubProducerClient;
+    
+    public EventHubClient(string connectionString)
+    {
+        m_eventHubProducerClient = new EventHubProducerClient(connectionString);
+    }
+
+    public async Task SendAsync(JArray jsonArray, CancellationToken cancellationToken = default)
+    {
+        using var eventBatch = await m_eventHubProducerClient.CreateBatchAsync(cancellationToken);
+        foreach (var jToken in jsonArray)
+        {
+            var jsonString = JsonConvert.SerializeObject(jToken);
+            eventBatch.TryAdd(new EventData(Encoding.Unicode.GetBytes(jsonString)));
+        }
+
+        await m_eventHubProducerClient.SendAsync(eventBatch, cancellationToken);
+    }
+}
